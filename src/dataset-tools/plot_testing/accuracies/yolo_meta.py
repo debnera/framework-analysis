54f39ff0d6fb03fa28bee61e26e74ed519f51306
Anton Debner <anton.debner@aalto.fi>
2024-11-25 11:44:37 +0200

    Add specs for all yolo models

---
+++ src/dataset-tools/plot_testing/accuracies/yolo_meta.py
@@ -0,0 +1,185 @@
+yolo_11 = [
+    {
+        "Model": "YOLO11n",
+        "size (pixels)": 640,
+        "mAP val 50-95": 39.5,
+        "Speed CPU ONNX (ms)": "56.1 ± 0.8",
+        "Speed T4 TensorRT10 (ms)": "1.5 ± 0.0",
+        "params (M)": 2.6,
+        "FLOPs (B)": 6.5
+    },
+    {
+        "Model": "YOLO11s",
+        "size (pixels)": 640,
+        "mAP val 50-95": 47.0,
+        "Speed CPU ONNX (ms)": "90.0 ± 1.2",
+        "Speed T4 TensorRT10 (ms)": "2.5 ± 0.0",
+        "params (M)": 9.4,
+        "FLOPs (B)": 21.5
+    },
+    {
+        "Model": "YOLO11m",
+        "size (pixels)": 640,
+        "mAP val 50-95": 51.5,
+        "Speed CPU ONNX (ms)": "183.2 ± 2.0",
+        "Speed T4 TensorRT10 (ms)": "4.7 ± 0.1",
+        "params (M)": 20.1,
+        "FLOPs (B)": 68.0
+    },
+    {
+        "Model": "YOLO11l",
+        "size (pixels)": 640,
+        "mAP val 50-95": 53.4,
+        "Speed CPU ONNX (ms)": "238.6 ± 1.4",
+        "Speed T4 TensorRT10 (ms)": "6.2 ± 0.1",
+        "params (M)": 25.3,
+        "FLOPs (B)": 86.9
+    },
+    {
+        "Model": "YOLO11x",
+        "size (pixels)": 640,
+        "mAP val 50-95": 54.7,
+        "Speed CPU ONNX (ms)": "462.8 ± 6.7",
+        "Speed T4 TensorRT10 (ms)": "11.3 ± 0.2",
+        "params (M)": 56.9,
+        "FLOPs (B)": 194.9
+    }
+]
+
+yolo_10 = [
+    {
+        "Model": "YOLOv10-N",
+        "Input Size": 640,
+        "APval": 38.5,
+        "FLOPs (G)": 6.7,
+        "Latency (ms)": 1.84
+    },
+    {
+        "Model": "YOLOv10-S",
+        "Input Size": 640,
+        "APval": 46.3,
+        "FLOPs (G)": 21.6,
+        "Latency (ms)": 2.49
+    },
+    {
+        "Model": "YOLOv10-M",
+        "Input Size": 640,
+        "APval": 51.1,
+        "FLOPs (G)": 59.1,
+        "Latency (ms)": 4.74
+    },
+    {
+        "Model": "YOLOv10-B",
+        "Input Size": 640,
+        "APval": 52.5,
+        "FLOPs (G)": 92.0,
+        "Latency (ms)": 5.74
+    },
+    {
+        "Model": "YOLOv10-L",
+        "Input Size": 640,
+        "APval": 53.2,
+        "FLOPs (G)": 120.3,
+        "Latency (ms)": 7.28
+    },
+    {
+        "Model": "YOLOv10-X",
+        "Input Size": 640,
+        "APval": 54.4,
+        "FLOPs (G)": 160.4,
+        "Latency (ms)": 10.70
+    }
+]
+
+yolo_9 = [
+    {
+        "Model": "YOLOv9t",
+        "size (pixels)": 640,
+        "mAPval 50-95": 38.3,
+        "mAPval 50": 53.1,
+        "params (M)": 2.0,
+        "FLOPs (B)": 7.7
+    },
+    {
+        "Model": "YOLOv9s",
+        "size (pixels)": 640,
+        "mAPval 50-95": 46.8,
+        "mAPval 50": 63.4,
+        "params (M)": 7.2,
+        "FLOPs (B)": 26.7
+    },
+    {
+        "Model": "YOLOv9m",
+        "size (pixels)": 640,
+        "mAPval 50-95": 51.4,
+        "mAPval 50": 68.1,
+        "params (M)": 20.1,
+        "FLOPs (B)": 76.8
+    },
+    {
+        "Model": "YOLOv9c",
+        "size (pixels)": 640,
+        "mAPval 50-95": 53.0,
+        "mAPval 50": 70.2,
+        "params (M)": 25.5,
+        "FLOPs (B)": 102.8
+    },
+    {
+        "Model": "YOLOv9e",
+        "size (pixels)": 640,
+        "mAPval 50-95": 55.6,
+        "mAPval 50": 72.8,
+        "params (M)": 58.1,
+        "FLOPs (B)": 192.5
+    }
+]
+
+
+
+yolo_8 = [
+    {
+        "Model": "YOLOv8n",
+        "size (pixels)": 640,
+        "mAP val 50-95": 37.3,
+        "Speed CPU ONNX (ms)": 80.4,
+        "Speed A100 TensorRT (ms)": 0.99,
+        "params (M)": 3.2,
+        "FLOPs (B)": 8.7
+    },
+    {
+        "Model": "YOLOv8s",
+        "size (pixels)": 640,
+        "mAP val 50-95": 44.9,
+        "Speed CPU ONNX (ms)": 128.4,
+        "Speed A100 TensorRT (ms)": 1.20,
+        "params (M)": 11.2,
+        "FLOPs (B)": 28.6
+    },
+    {
+        "Model": "YOLOv8m",
+        "size (pixels)": 640,
+        "mAP val 50-95": 50.2,
+        "Speed CPU ONNX (ms)": 234.7,
+        "Speed A100 TensorRT (ms)": 1.83,
+        "params (M)": 25.9,
+        "FLOPs (B)": 78.9
+    },
+    {
+        "Model": "YOLOv8l",
+        "size (pixels)": 640,
+        "mAP val 50-95": 52.9,
+        "Speed CPU ONNX (ms)": 375.2,
+        "Speed A100 TensorRT (ms)": 2.39,
+        "params (M)": 43.7,
+        "FLOPs (B)": 165.2
+    },
+    {
+        "Model": "YOLOv8x",
+        "size (pixels)": 640,
+        "mAP val 50-95": 53.9,
+        "Speed CPU ONNX (ms)": 479.1,
+        "Speed A100 TensorRT (ms)": 3.53,
+        "params (M)": 68.2,
+        "FLOPs (B)": 257.8
+    }
+]
